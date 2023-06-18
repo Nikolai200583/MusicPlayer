3cbc030165bd332f93c834486bd3fb4c895adf01
Nikolai <bonum_trd@bk.ru>
2023-06-18 12:48:43 +0200

    test getApi

--- src/redux/tests/api.test.js
+++ src/redux/tests/api.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import '@testing-library/jest-dom'
 import { setupApiStore } from "./test.utils";
 import { ItemsAuthor } from "../../components/PlayListItems/ItemsAuthor";
 import { render, screen } from "@testing-library/react";
@@ -9,9 +9,9 @@ import { musicApi } from "../musicApi";
 
 
 export const handlers = [
-  rest.get('https://painassasin.online/catalog/track/8/', (res, ctx) => {
+  rest.get('https://painassasin.online/catalog/track/all/', (res, ctx) => {
     return res(
-      ctx.json([{ author: "Alexander Nakarada" }])
+      ctx.json({ author: "Alexander Nakarada" })
       
     );
   }),
@@ -19,7 +19,6 @@ export const handlers = [
 
 
 const server = setupServer(...handlers);
-
 const storeRef = setupApiStore(musicApi);
 
 describe("Playlist feature", () => {  
@@ -28,7 +27,9 @@ describe("Playlist feature", () => {
   afterAll(() => server.close());
 
   it("should show requested data", async () => {
-    render(<ItemsAuthor />, { wrapper: storeRef.wrapper });   
+    render(<ItemsAuthor author = "Alexander Nakarada" />,
+     { wrapper: storeRef.wrapper });  
+
     screen.getByText("Loading...");    
     expect(await screen.findByText(/Alexander Nakarada/i)).toBeInTheDocument();
     expect(screen.queryByText(/loading/i)).not.toBeInTheDocument();
